07cefb95e9f12d259a9fdfe31b818f2c2f016706
Franck le Frustré <franck.laurent07@orange.fr>
2023-01-10 14:09:47 +0100

    footerfinal

--- assets/php/partials/_footer.php
+++ assets/php/partials/_footer.php
@@ -1,21 +1,18 @@
 
-<footer class="footergeneral">
-      <div class="mentions">Mentions Légales</div>
-      <div class="contacts">Nous contacter</div>
+  <footer class="footergeneral">
+      <div class="mentions"><a href="#" class="choixbieres">Mentions Légales</a></div>
+      <div class="contacts"><a href="#" class="choixbieres" >Nous contacter</a></div>
       <div class="logofooter"></div>
       <div class="questionbieres">
-        <a class="choixbieres">Comment bien choisir sa bière?</a>
+        <a class="choixbieres"> Comment bien choisir sa bière?</a>
       </div>
       <div class="logoreseauxsociaux">
         <div class="blocrs">Réseaux Sociaux</div>
         <div class="reseaux" >
-          <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-snapchat-ghost" aria-hidden="true"></i></a>
+          <a href="#" class="fa fa-facebook"></a><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a><a href="#"><i class="fa fa-snapchat-ghost" aria-hidden="true"></i></a>
       </div>
       </div>
-      </div>
+      
     </footer>
      
   </body>
